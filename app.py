dddcc3599ebf6e2b64a1aab79f800ccaaa12f848
Fu Lim <fulim1130@gmail.com>
2024-05-11 02:01:25 +0800

    h5

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ recommendation_model.eval()
 
 # Streamlit app
 st.title("Movie Recommendation System")
-st.markdown("#### Recommending movies to users using a Neural Network Collaborative Filtering model.")
+st.markdown("##### Recommending movies to users using a Neural Network Collaborative Filtering model.")
 
 # user_id = st.selectbox("Select a user ID", df["userId"].unique())
 user_id = st.sidebar.selectbox("Select a user ID", df["userId"].unique())
